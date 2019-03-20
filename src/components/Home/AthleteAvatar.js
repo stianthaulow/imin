fd3ee123d6e71b1e74627f036645774acc5f4307
Thaulow <stian@stianthaulow.com>
2019-03-20 22:12:27 +0100

    Add default icon for avatars

--- src/components/Home/AthleteAvatar.js
+++ src/components/Home/AthleteAvatar.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 
 const AthleteAvatar = props => {
   const { name, img } = props;
-  return <Avatar alt={name} src={img} />;
+  return img ? (
+    <Avatar alt={name} src={img} />
+  ) : (
+    <Avatar alt={name}>
+      <PermIdentityIcon />
+    </Avatar>
+  );
 };
 
 export default AthleteAvatar;
