1f2d34e7f06ae274e0b818726856e35e30bcf305
Thaulow <stian@stianthaulow.com>
2019-03-24 16:14:14 +0100

    Add production environment for master

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 
 import App from "./components/App";
 
 Sentry.init({
-  dsn: "https://c8cfde08880e4905bd25b36678c57364@sentry.io/1419100"
+  dsn: "https://c8cfde08880e4905bd25b36678c57364@sentry.io/1419100",
+  environment: "production"
 });
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
